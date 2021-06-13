3cab5391a48d8194f74eb2adb55702ecc9d2a0e5
MrBo88 <Mr@Bo88.io>
2021-06-13 18:24:08 +0300

    icons for skills

--- src/skills/Skill.js
+++ src/skills/Skill.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import styles from './Skills.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Skill(props) {
   return (
     <div className={styles.skill}>
-      <div className={styles.skillIcon}>Icon</div>
+      <div className={styles.skillIcon}>
+        <img src={props.icon} alt='icon' />
+      </div>
       <h3 className={styles.skillTitle}>{props.title}</h3>
-      <p className={styles.skillDescription}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem
-        Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
-        type and scrambled it to make a type specimen book.</p>
+      <p className={styles.skillDescription}>{props.text}</p>
     </div>
   )
 }
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import styles from './Skills.module.css'
 import Skill from './Skill'
+import TypeScriptIcon from '../icons/typescript.png'
+import ReactIcon from '../icons/react.png'
+import JSIcon from '../icons/javascript.png'
+import CSSIcon from '../icons/css.png'
+import HTML5Icon from '../icons/html-5.png'
 
 function Skills() {
   return (
@@ -10,11 +15,33 @@ function Skills() {
           <h2>My Skills</h2>
         </div>
         <div className={styles.skillsWrapper}>
-          <Skill title={'HTML'} />
-          <Skill title={'CSS'} />
-          <Skill title={'JS'} />
-          <Skill title={'React'} />
-          <Skill title={'TypeScript'} />
+          <Skill
+            title={'HTML'}
+            icon={HTML5Icon}
+            text={
+              'HTML5 is a markup language used for structuring and presenting content on the World Wide Web. It is the fifth and last major HTML version that is a World Wide Web Consortium (W3C) recommendation'
+            }
+          />
+          <Skill
+            title={'CSS'}
+            icon={CSSIcon}
+            text='Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript'
+          />
+          <Skill
+            title={'JS'}
+            icon={JSIcon}
+            text='JavaScript ,often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions'
+          />
+          <Skill
+            title={'React'}
+            icon={ReactIcon}
+            text='React is an open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications'
+          />
+          <Skill
+            title={'TypeScript'}
+            icon={TypeScriptIcon}
+            text='TypeScript is a programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript and adds optional static typing to the language. TypeScript is designed for the development of large applications and transcompiles to JavaScript'
+          />
         </div>
       </div>
     </section>
